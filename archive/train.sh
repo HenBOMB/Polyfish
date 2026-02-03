4e65de572ff0894898205095b71b080e86129f58
HenBOMB <alan.io.nightcode@gmail.com>
2026-02-03 19:38:56 -0300

    commit

--- archive/train.sh
+++
@@ -1,5 +0,0 @@
-.venv/bin/python polyfish/trainer.py \
- --games 20 --simulations 200 --prefix "" --epochs 20 \
- --cpuct 1.5 --gamma 0.997 --rollouts 20 --temperature 0.9  \
- --mapsize 9 --tribes "Imperius,Imperius" --dirichlet false \
-&& clear && tail -f training.log
--- archive/ts_simulator/src/core/moves/Ability.ts
+++
@@ -1,8 +0,0 @@
-import Move from "../move";
-import { MoveType } from "../types";
-
-export default class Ability extends Move {
-    constructor(src: number | null, target: number | null, type: number | null) {
-        super(MoveType.Ability, src, target, type);
-    }
-}
