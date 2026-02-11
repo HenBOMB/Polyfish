edf374cb24ab6525c22c5e576288659701916951
HenBOMB <alan.io.nightcode@gmail.com>
2026-02-11 15:54:58 -0300

    commit

--- polyfish-rs/run_training_loop.sh
+++ polyfish-rs/run_training_loop.sh
@@ -3,7 +3,7 @@ set -e
 
 # Configuration
 ITERATIONS=100
-GAMES_PER_ITER=7
+GAMES_PER_ITER=20
 export MCTS_ITERS=200 # 200 = Optimized for RunPod GPU (~0.8s per move)
 export RAYON_NUM_THREADS=12
 export OMP_NUM_THREADS=12
