6f9c656306d634c25e8c9e23fb5225bc46491e1e
HenBOMB <alan.io.nightcode@gmail.com>
2026-05-31 12:29:44 -0300

    training: 200 iters

---
+++ polyfish-rs/scratch.rs
@@ -0,0 +1,10 @@
+use polyfish::game::Game;
+use polyfish::mapgen::{generate, MapGenSettings};
+use polyfish::moves::{AttackMove, Move};
+use polyfish::replayer::{ModReplay, ReplayPlayer, ReplayTurn};
+use polyfish::types::{MapSize, MapType, TribeType, UnitType};
+use polyfish::states::GameState;
+
+fn main() {
+    println!("Hello, World!");
+}
